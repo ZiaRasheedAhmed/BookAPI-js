579cc9ab431824891e1cd5765977c1a805e9884d
ZiaRasheedAhmed <zia.rasheed@xloopdigital.com>
2022-12-02 17:23:07 +0500

    AC 2 test failed

--- book.js
+++ book.js
@@ -1,9 +1,10 @@
 
 let arr = new Array();
-// arr[0] = ("The Hobbit by J.R.R","Tolkein",320,1937);
+arr[0] = ("The Hobbit by J.R.R","Tolkein",320,1937);
 
 function getbook(){
     return arr.length;
 }
-
+console.log(arr.length);
 module.exports = getbook;
+
