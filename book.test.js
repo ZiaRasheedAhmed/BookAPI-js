d295368faf950c997c01e6308f85dee6094c8cbf
ZiaRasheedAhmed <zia.rasheed@xloopdigital.com>
2022-12-02 18:03:37 +0500

    AC 1, AC 2 passed and AC3 failed

--- book.test.js
+++ book.test.js
@@ -17,3 +17,14 @@ test('Empty List', () => {
         //Assert
         expect(O.getbook()).toBe(1);
     });
+
+
+// 3. Given that I have an empty list, when I add additional books to the list I expect the numberRead to return the total number of books in my list.
+test('add additional book and return 2', () => {
+    //Act
+    var b = new O.Book("The Hobbit","J.R.R. Tolkein",320,1937);
+    var b = new O.Book("Alices Adventures in Wonderland","Lewis Carroll",544,1865);
+    O.addbook(b);
+    //Assert
+    expect(O.getbook()).toBe(1);
+});
